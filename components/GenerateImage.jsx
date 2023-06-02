df86566c9b09d2e0405525aaf471a554047afbcd
ariztegui8 <jorgeariztegui8@gmail.com>
2023-06-02 15:04:41 -0300

    cambios api

--- components/GenerateImage.jsx
+++ components/GenerateImage.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Configuration, OpenAIApi } from "openai";
 
 const GenerateImage = () => {
 
+  const [image, setImage] = useState(null)
+
     const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
+        apiKey: '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',
       });
       const openai = new OpenAIApi(configuration);
 
       const generateImg = async ()=>{
         const res = await openai.createImage({
-            prompt: "gato negro",
+            prompt: "caballo blanco",
             n: 1,
             size: "512x512",
         });
 
         console.log(res.data.data[0].url);
+        setImage(res.data.data[0].url);
       }
 
   return (
     <div>
         <button onClick={generateImg} className="btn">Generate</button>
+        <img src={image} alt="image" />
     </div>
   )
 }
